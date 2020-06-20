ff06508c1914daf496a11edbe789b4c6fad9eccf
Tiago Padilha <tiagospem@gmail.com>
2020-06-19 21:11:44 -0300

    alterado o spinner preload

--- resources/js/components/spinner/index.js
+++ resources/js/components/spinner/index.js
@@ -1,5 +1,22 @@
 import React from 'react'
+import ReactBodymovin from 'react-bodymovin'
+import lottie404 from '../../components/animations/7774-loading.json'
+import {Col, Container, Row} from "reactstrap"
 
 export default function Spinner(){
-    return ('Loading....')
+    const bodyMovieOptions = {
+        loop: true,
+        autoplay: true,
+        prerender: true,
+        animationData: lottie404
+    }
+    return (
+        <Container className={'h-100'}>
+            <Row className={'h-100 justify-content-center align-items-center'}>
+                <Col md={3}>
+                    <ReactBodymovin options={bodyMovieOptions} />
+                </Col>
+            </Row>
+        </Container>
+    )
 }
